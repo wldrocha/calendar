78aaeb5f4f1f4282d91aacadbc78e79ab9bc7516
Wladimir Rocha <wld.rocha@gmail.com>
2022-09-25 21:11:35 -0400

    [REF] components

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,20 +1,8 @@
-import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
+import { Calendar } from 'react-big-calendar'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
-import { addHours, format, parse, startOfWeek, getDay } from 'date-fns'
-import enUS from 'date-fns/locale/en-US'
+import { localizer, getMessageES } from '../../helpers'
+import { addHours } from 'date-fns'
 import { Navbar } from '../components/Navbar'
-
-const locales = {
-  'en-US': enUS
-}
-
-const localizer = dateFnsLocalizer({
-  format,
-  parse,
-  startOfWeek,
-  getDay,
-  locales
-})
 
 const events = [
   {
@@ -27,15 +15,34 @@ const events = [
 ]
 
 export const CalendarPage = () => {
+  const eventStyleGetter = (event, start, end, isSelected) => {
+    console.log('🚀 ~ file: CalendarPage.jsx ~ line 20 ~ eventStyleGetter ~ event, start, end, isSelected)', {
+      event,
+      start,
+      end,
+      isSelected
+    })
+    const style = {
+      backgroundColor: '#347CF7',
+      borderRadius: '0px',
+      opacity: '0.8',
+      color: 'white'
+    }
+
+    return { style }
+  }
   return (
     <>
       <Navbar />
       <Calendar
+        culture={'es'}
         localizer={localizer}
         events={events}
         startAccessor='start'
         endAccessor='end'
         style={{ height: 'calc(100vh - 80px)' }}
+        messages={getMessageES()}
+        eventPropGetter={eventStyleGetter}
       />
     </>
   )
